036ca24502a176e6a6bcc1edd06c8ca1e1f97e83
David Ong <dodwei.dev@gmail.com>
2021-03-08 17:35:13 +0800

    Gatsby Tutorial #3 - Pages & Routes

---
+++ src/pages/about.js
@@ -0,0 +1,27 @@
+import React from "react"
+
+export default function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
+        veritatis nesciunt laborum culpa autem! Ullam obcaecati temporibus
+        eveniet architecto blanditiis deleniti voluptatum ducimus soluta
+        asperiores, natus nostrum nam iusto consectetur!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
+        veritatis nesciunt laborum culpa autem! Ullam obcaecati temporibus
+        eveniet architecto blanditiis deleniti voluptatum ducimus soluta
+        asperiores, natus nostrum nam iusto consectetur!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
+        veritatis nesciunt laborum culpa autem! Ullam obcaecati temporibus
+        eveniet architecto blanditiis deleniti voluptatum ducimus soluta
+        asperiores, natus nostrum nam iusto consectetur!
+      </p>
+    </div>
+  )
+}
