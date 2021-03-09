1c4ea8380ba3f25a8982b9032e243a1744f04473
David Ong <dodwei.dev@gmail.com>
2021-03-09 15:57:15 +0800

    Gatsby Tutorial #5 - Creating a Layout File

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,27 +1,30 @@
 import React from "react"
+import Layout from "../components/Layout"
 
 export default function About() {
   return (
-    <div>
-      <h1>About Page</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
-        veritatis nesciunt laborum culpa autem! Ullam obcaecati temporibus
-        eveniet architecto blanditiis deleniti voluptatum ducimus soluta
-        asperiores, natus nostrum nam iusto consectetur!
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
-        veritatis nesciunt laborum culpa autem! Ullam obcaecati temporibus
-        eveniet architecto blanditiis deleniti voluptatum ducimus soluta
-        asperiores, natus nostrum nam iusto consectetur!
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
-        veritatis nesciunt laborum culpa autem! Ullam obcaecati temporibus
-        eveniet architecto blanditiis deleniti voluptatum ducimus soluta
-        asperiores, natus nostrum nam iusto consectetur!
-      </p>
-    </div>
+    <Layout>
+      <div>
+        <h1>About Page</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
+          veritatis nesciunt laborum culpa autem! Ullam obcaecati temporibus
+          eveniet architecto blanditiis deleniti voluptatum ducimus soluta
+          asperiores, natus nostrum nam iusto consectetur!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
+          veritatis nesciunt laborum culpa autem! Ullam obcaecati temporibus
+          eveniet architecto blanditiis deleniti voluptatum ducimus soluta
+          asperiores, natus nostrum nam iusto consectetur!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
+          veritatis nesciunt laborum culpa autem! Ullam obcaecati temporibus
+          eveniet architecto blanditiis deleniti voluptatum ducimus soluta
+          asperiores, natus nostrum nam iusto consectetur!
+        </p>
+      </div>
+    </Layout>
   )
 }
