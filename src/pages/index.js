399fc0f3d5f58efdd89cf6586a38b5dcdd85065c
David Ong <dodwei.dev@gmail.com>
2021-03-15 17:12:01 +0800

    Gatsby Tutorial #6 - Styling Components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,19 @@
 import React from "react"
-import Navbar from "../components/Navbar"
+import { Link } from "gatsby"
 import Layout from "../components/Layout"
+import { header, btn } from "../styles/home.module.css"
 
 export default function Home() {
   return (
     <Layout>
-      <section>
+      <section className={header}>
         <div>
           <h2>Design</h2>
           <h3>Develop & Deploy</h3>
           <p>UX designer & web developer based in Manchester.</p>
+          <Link className={btn} to="/projects">
+            My Portfolio Projects
+          </Link>
         </div>
       </section>
     </Layout>
